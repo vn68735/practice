866df98d47b393fa70bb057be31b8686302cb118
venkata.vikas <50772815+vn68735@users.noreply.github.com>
2022-01-27 16:56:47 -0800

    Add files via upload

---
+++ 1.24.py
@@ -0,0 +1,45 @@
+"""str=' python '
+print(len(str))
+print(str.upper())
+print(str.lower())
+print(str.strip())
+print(len(str))
+for i in range(1,10):
+    print(i)
+print(3/2)
+print(3//2)
+print(3%2)
+print(9**2)
+a=3
+b=2
+print(a==b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+if a>b:
+    print('do some task')
+str1='python'
+str2='program'
+print(len(str1)==len(str2))
+if len(str1)!=len(str2):
+    print('they are not same')
+   
+sem1='pass'
+sem2='fail'
+print(sem1=='pass' and sem2=='pass')
+print(sem1=='pass' or sem2=='pass')
+print(not sem2=='pass')
+
+if (sem1=='pass' or sem2=='pass'):
+    print('promot him to next class')
+   
+a=2
+b=3
+print(a & b) #bitwise and operator
+print(a|b) #bitwise and operator
+a=2
+print(a+=2)
+a=[1,2,3]
+if 3 in a:
+    print('present')"""
